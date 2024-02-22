f81bf8c04550dc84d591048a2778d3f580d10b7b
Paul Adamski <pauladamski1985@gmail.com>
2024-02-22 22:00:11 +0100

    update

--- src/utils/jwtCheck.js
+++ src/utils/jwtCheck.js
@@ -1,5 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
+// TODO: Add your own client
+
 const jwtCheck = auth({
   audience: "https://booking-api",
   issuerBaseURL: "https://dev-ja55wjcl2iwpgu13.eu.auth0.com/",
