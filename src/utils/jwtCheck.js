ac0d9a5fc9eee4d1dcaaeee018e2efaa9389c39e
Paul Adamski <pauladamski1985@gmail.com>
2024-02-23 12:43:13 +0100

    update

--- src/utils/jwtCheck.js
+++ src/utils/jwtCheck.js
@@ -2,10 +2,27 @@ import { auth } from "express-oauth2-jwt-bearer";
 
 // TODO: Add your own client
 
+const express = require("express");
+const app = express();
+const { auth } = require("express-oauth2-jwt-bearer");
+
+const port = process.env.PORT || 8080;
+
 const jwtCheck = auth({
   audience: "https://booking-api",
-  issuerBaseURL: "https://dev-ja55wjcl2iwpgu13.eu.auth0.com/",
+  issuerBaseURL: "https://dev-shhvbp4uvq37sfw3.us.auth0.com/",
   tokenSigningAlg: "RS256",
 });
 
-export default jwtCheck;
+// enforce on all endpoints
+app.use(jwtCheck);
+
+app.get("/authorized", function (req, res) {
+  res.send("Secured Resource");
+});
+
+app.listen(port);
+
+console.log("Running on port ", port);
+
+export default jwt;
